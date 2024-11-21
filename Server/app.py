c3be746266dc6f41548a32610e081c5647700448
isaacwallace123 <goosewal@gmail.com>
2024-11-21 16:20:27 -0500

    Added testing routes for front-end and api

--- Server/app.py
+++ Server/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, send_from_directory, jsonify
+from flask_cors import CORS
+
 import paho.mqtt.client as paho
 
 def on_connect(client, userdata, flags, rc):
@@ -16,13 +18,22 @@ client.connect("192.168.1.2", 1883, 60)
 
 client.loop_start()
 
+
 # Web Service
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='../App/dist', static_url_path='')
+CORS(app)
 
 @app.route('/')
 def index():
-    return jsonify({})
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({
+        "John": True
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
